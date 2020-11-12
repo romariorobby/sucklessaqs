54924ef0d40e7a2076b7b40e61178a8a238432c4
romariorobby <romariorobby@yahoo.com>
2020-11-12 23:44:47 +0700

    adding swallow and scratchpads

--- dwm/testdwm/config.h
+++ dwm/testdwm/config.h
@@ -55,6 +55,7 @@ static const Rule rules[] = {
 	{ "Gimp",             NULL,          NULL,           0,              1,          0,           0,        -1 },
 	{ "Firefox",          NULL,          NULL,           1 << 8,         0,          0,          -1,        -1 },
 	{ "brave-browser",    NULL,          NULL,           1 << 8,         0,          0,          -1,        -1 },
+	{ "st",               NULL,          NULL,           0,              0,          1,           0,        -1 },
 	{ "St",               NULL,          NULL,           0,              0,          1,           0,        -1 },
 	{ NULL,               NULL,          "Event Tester", 0,              0,          0,           1,        -1 }, /* xev */
 	{ NULL,		          "spterm",	     NULL,		     SPTAG(0),		 1,		     1,           0,        -1 },
--- dwm/testdwm/dwm.c
+++ dwm/testdwm/dwm.c
@@ -928,13 +928,13 @@ destroynotify(XEvent *e)
 
 	if ((c = wintoclient(ev->window)))
 		unmanage(c, 1);
+		// TODO check again if this code below works?
+	else if ((c = swallowingclient(ev->window)))
+		unmanage(c->swallowing, 1);
 	else if ((c = wintosystrayicon(ev->window))) {
 		removesystrayicon(c);
 		resizebarwin(selmon);
 		updatesystray();
-		// TODO check again if this code below works?
-	else if ((c = swallowingclient(ev->window)))
-		unmanage(c->swallowing, 1);
 	}
 }
 
