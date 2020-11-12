1d2bad2da6cd1e84bb5ccd543f62f4ef240d668c
romariorobby <romariorobby@yahoo.com>
2020-11-12 21:42:10 +0700

    change bind 'defaultgaps'

--- dwm/testdwm/config.h
+++ dwm/testdwm/config.h
@@ -116,7 +116,7 @@ static Key keys[] = {
 //	{ MODKEY|Mod4Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
 //	{ MODKEY|Mod4Mask,              XK_9,      incrovgaps,     {.i = +1 } },
 //	{ MODKEY|Mod4Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
-	{ MODKEY|ShiftMask|ControlMask, XK_BackSpace,      defaultgaps,    {0} },
+	{ MODKEY|ControlMask,           XK_BackSpace,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
