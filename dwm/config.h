e99ca5a07381b419f4d016dde4ebed891c64620f
romariorobby <romariorobby@yahoo.com>
2020-11-14 22:41:25 +0700

    add hacks fonts

--- dwm/config.h
+++ dwm/config.h
@@ -18,7 +18,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "Hasklug Nerd Font:size=10", "Siji:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static const char *fonts[]          = { "monospace:size=10","Hack Nerd Font:size=10", "Hasklug Nerd Font:size=10", "Siji:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_bg[]          = "#141414";
--- yadav-dwmblocks/blocks.h
+++ yadav-dwmblocks/blocks.h
@@ -1,7 +1,7 @@
 /* time interval in seconds to sleep before looking for updates in the main loop */
 #define SLEEPINTERVAL                   1
 
-#define PATH(name)                      "/home/mario/.local/bin/blocks/"name
+#define PATH(name)                      "/home/mariolv/.local/bin/blocks/"name
 
 /* If interval of a block is set to 0, the block will only be updated once at startup.
  * If interval is set to a negative value, the block will never be updated in the main loop.
@@ -24,7 +24,7 @@ static Block blocks[] = {
 
         { PATH("cputemp"),           PATH("cputemp"),                    1,              3},
 
-//        { PATH("battery"),           PATH("battery_button"),              30,             2},
+        { PATH("battery"),           PATH("battery"),                    30,             2},
 
         { PATH("clock"),             PATH("clock"),                      1,              1},
 
@@ -33,4 +33,4 @@ static Block blocks[] = {
         { NULL } /* just to mark the end of the array */
 };
 
-static const char *delim =  "  ";
+static const char *delim =  " ";
