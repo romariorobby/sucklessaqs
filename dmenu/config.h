6b2f5d80cea81e379afbfb4103fd86172b1b1ba4
romariorobby <romariorobby@yahoo.com>
2020-11-22 21:35:21 +0700

    update dmenu

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,12 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=10",
+	"JoyPixels:pixelsize=10:antialias=true:autohint=true",
+	"Hasklug Nerd Font:size=10",
+	"Siji:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeNorm] = { "#bbbbbb", "#141414" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
